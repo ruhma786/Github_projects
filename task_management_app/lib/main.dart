282f8479d302d4037ad29b41d15fc55bd8afc1c8
ruhma786 <ruhmazulfiqar2002@gmail.com>
2024-10-31 22:34:26 +0500

    update file

--- task_management_app/lib/main.dart
+++ task_management_app/lib/main.dart
@@ -17,8 +17,54 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Task Management App',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.teal,
+          scaffoldBackgroundColor: Colors.grey[100],
+          textTheme: TextTheme(
+            titleLarge: TextStyle(
+              color: Colors.teal[700],
+              fontSize: 26,
+              fontWeight: FontWeight.bold,
+            ),
+            bodyMedium: TextStyle(
+              color: Colors.black87,
+            ),
+          ),
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.teal[600],
+            titleTextStyle: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+            ),
+            elevation: 4,
+          ),
+          buttonTheme: ButtonThemeData(
+            buttonColor: Colors.teal[400],
+            textTheme: ButtonTextTheme.primary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.white, backgroundColor: Colors.teal[300],
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+          ),
+          cardTheme: CardTheme(
+            color: Colors.white,
+            shadowColor: Colors.teal[100],
+            elevation: 5,
+            margin: EdgeInsets.all(8),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
         ),
+        debugShowCheckedModeBanner: false,
         home: TaskScreen(),
       ),
     );
