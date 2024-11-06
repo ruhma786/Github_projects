15709b18590cb9d05f24376dce3de91c9a65dbed
ruhma786 <ruhmazulfiqar2002@gmail.com>
2024-11-07 00:27:48 +0500

    add the db_helper file for database

---
+++ task_management_app/lib/db_helper.dart
@@ -0,0 +1,52 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DBHelper {
+  static Database? _db;
+
+  Future<Database> get database async {
+    if (_db != null) return _db!;
+    _db = await initDB();
+    return _db!;
+  }
+
+  Future<Database> initDB() async {
+    String path = join(await getDatabasesPath(), 'task_manager.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE tasks(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            description TEXT,
+            dueDate TEXT,
+            category TEXT,
+            isCompleted INTEGER
+          )
+        ''');
+      },
+    );
+  }
+
+  Future<int> insertTask(Map<String, dynamic> task) async {
+    final db = await database;
+    return await db.insert('tasks', task);
+  }
+
+  Future<List<Map<String, dynamic>>> getTasks() async {
+    final db = await database;
+    return await db.query('tasks');
+  }
+
+  Future<int> updateTask(int id, Map<String, dynamic> task) async {
+    final db = await database;
+    return await db.update('tasks', task, where: 'id = ?', whereArgs: [id]);
+  }
+
+  Future<int> deleteTask(int id) async {
+    final db = await database;
+    return await db.delete('tasks', where: 'id = ?', whereArgs: [id]);
+  }
+}
