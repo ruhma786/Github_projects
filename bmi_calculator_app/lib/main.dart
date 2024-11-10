3fc037026daac3a2a3e973026cb18d1e21c52fb7
ruhma786 <ruhmazulfiqar2002@gmail.com>
2024-11-10 22:38:44 +0500

    video#9
    use  GestureDetector and on tap function

--- bmi_calculator_app/lib/main.dart
+++ bmi_calculator_app/lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 import 'input.dart';
+import 'icon.dart';
+import 'container.dart';
 void main() {
   runApp(BMICalculatorApp());
 }
-
 class BMICalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-            home: BMICalculatorScreen(),
-      theme: ThemeData(
+      debugShowCheckedModeBanner: false,
+      home: BMICalculatorScreen(),
+      theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
-       textTheme: TextTheme(
-         bodyLarge : TextStyle(color: Colors.white)
-       )
       ),
     );
   }
 }
+
+
+
