5436f56381a9d4cb3f6aca57b9a253818d48951e
ruhma786 <ruhmazulfiqar2002@gmail.com>
2024-11-10 22:10:08 +0500

    video#6
    create different files

---
+++ bmi_calculator_app/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'input.dart';
+void main() {
+  runApp(BMICalculatorApp());
+}
+
+class BMICalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+            home: BMICalculatorScreen(),
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+       textTheme: TextTheme(
+         bodyLarge : TextStyle(color: Colors.white)
+       )
+      ),
+    );
+  }
+}
