6a1fd8dcdd284fab4436763ec8b3228b750edad0
ruhma786 <ruhmazulfiqar2002@gmail.com>
2024-11-10 19:59:12 +0500

    video#3
    in this video we learn about theme properties (themedata, textTheme) and textstyle etc and also make separate file (addition of new input.dart file  )

---
+++ bmi_calculator_app/lib/input.dart
@@ -0,0 +1,304 @@
+import 'package:flutter/material.dart';
+class BMICalculatorScreen extends StatefulWidget {
+  @override
+  _BMICalculatorScreenState createState() => _BMICalculatorScreenState();
+}
+
+class _BMICalculatorScreenState extends State<BMICalculatorScreen> {
+  bool isMale = true;
+  double height = 180.0;
+  int weight = 60;
+  int age = 20;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isMale = true;
+                      });
+                    },
+                    child: GenderCard(
+                      label: 'MALE',
+                      icon: Icons.male,
+                      isSelected: isMale,
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isMale = false;
+                      });
+                    },
+                    child: GenderCard(
+                      label: 'FEMALE',
+                      icon: Icons.female,
+                      isSelected: !isMale,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all(15.0),
+              decoration: BoxDecoration(
+                color: Color(0xFF1D1E33),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'HEIGHT',
+                    style: TextStyle(fontSize: 18.0, color: Colors.white),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        height.toStringAsFixed(1),
+                        style: TextStyle(
+                          fontSize: 50.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        ' cm',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  Slider(
+                    value: height,
+                    min: 120.0,
+                    max: 220.0,
+                    onChanged: (double newValue) {
+                      setState(() {
+                        height = newValue;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: CounterCard(
+                    label: 'WEIGHT',
+                    value: weight,
+                    onIncrement: () {
+                      setState(() {
+                        weight++;
+                      });
+                    },
+                    onDecrement: () {
+                      setState(() {
+                        weight--;
+                      });
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: CounterCard(
+                    label: 'AGE',
+                    value: age,
+                    onIncrement: () {
+                      setState(() {
+                        age++;
+                      });
+                    },
+                    onDecrement: () {
+                      setState(() {
+                        age--;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: double.infinity,
+            color: Color(0xFFEB1555),
+            margin: EdgeInsets.only(top: 10.0),
+            height: 80.0,
+            child: TextButton(
+              onPressed: () {
+                double bmi = weight / ((height / 100) * (height / 100));
+                String result;
+                if (bmi >= 25) {
+                  result = 'Overweight';
+                } else if (bmi > 18.5) {
+                  result = 'Normal';
+                } else {
+                  result = 'Underweight';
+                }
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text('Your BMI Result'),
+                    content: Text('BMI: ${bmi.toStringAsFixed(1)}\nResult: $result'),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.pop(context),
+                        child: Text('OK'),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              child: Text(
+                'CALCULATE',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GenderCard extends StatelessWidget {
+  final String label;
+  final IconData icon;
+  final bool isSelected;
+
+  GenderCard({required this.label, required this.icon, required this.isSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: isSelected ? Color(0xFF1D1E33) : Color(0xFF111328),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            icon,
+            size: 80.0,
+            color: Colors.white,
+          ),
+          SizedBox(height: 15.0),
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CounterCard extends StatelessWidget {
+  final String label;
+  final int value;
+  final VoidCallback onIncrement;
+  final VoidCallback onDecrement;
+
+  CounterCard({
+    required this.label,
+    required this.value,
+    required this.onIncrement,
+    required this.onDecrement,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 18.0,
+              color: Colors.white,
+            ),
+          ),
+          Text(
+            value.toString(),
+            style: TextStyle(
+              fontSize: 50.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RoundIconButton(
+                icon: Icons.remove,
+                onPressed: onDecrement,
+              ),
+              SizedBox(width: 10.0),
+              RoundIconButton(
+                icon: Icons.add,
+                onPressed: onIncrement,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RoundIconButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  RoundIconButton({required this.icon, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: onPressed,
+      elevation: 6.0,
+      constraints: BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
+      child: Icon(icon, color: Colors.white),
+    );
+  }
+}
