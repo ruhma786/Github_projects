965b76e2e070750ac1d61dfae44cb25495b83124
ruhma786 <ruhmazulfiqar2002@gmail.com>
2024-11-24 21:06:28 +0500

    video#01(second part)
    Introduction of Weather App https://www.youtube.com/watch?v=tDb-fDtoYxw&list=PL9xYeVPGEGzf1bLLAe4_KUARZZW3bIVWu&index=63

---
+++ weather_app/lib/climate.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class Climate extends StatefulWidget {
+  const Climate({super.key});
+  @override
+  State<Climate> createState() => _ClimateState();
+}
+
+class _ClimateState extends State<Climate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
