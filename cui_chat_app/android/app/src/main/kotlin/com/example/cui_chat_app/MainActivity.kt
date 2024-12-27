f1d9de614895f615b8cfde1bc10e2895a08d8525
ruhma786 <ruhmazulfiqar2002@gmail.com>
2024-12-27 18:03:14 +0500

    remove this projects

--- cui_chat_app/android/app/src/main/kotlin/com/example/cui_chat_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cui_chat_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- cuichatapp/android/app/src/main/kotlin/com/example/cuichatapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.cuichatapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
